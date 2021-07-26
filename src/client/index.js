1b51e0ec0debc29aef36d3448eb6e09f5a3c4cb3
Andreas Vieten <cryptonari@outlook.com>
2021-07-26 12:56:55 +0200

    feat: Babel loader JS

---
+++ src/client/index.js
@@ -0,0 +1,4 @@
+import { test_function } from "./js/formHandler";
+
+export {test_function}
+alert('I exist');
---
+++ src/client/js/formHandler.js
@@ -0,0 +1,5 @@
+const test_function = () => {
+    alert('This is a test');    
+}
+
+export {test_function}

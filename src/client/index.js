240d27de84380bfe7df9c339c70f6974d0d2a32b
Andreas Vieten <cryptonari@outlook.com>
2021-07-26 13:42:57 +0200

    feat: Sass Setup

--- src/client/index.js
+++ src/client/index.js
@@ -1,4 +1,10 @@
 import { test_function } from "./js/formHandler";
 
+import './styles/base.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/header.scss'
+import './styles/resets.scss'
+
 export {test_function}
 alert('I exist');

88863def9346f1290e04331071f0efa6ab0addf5
Andreas Vieten <cryptonari@outlook.com>
2021-07-26 13:24:51 +0200

    feat: webpack dev/prod server init

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,6 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     entry: './src/client/index.js',
+    mode: 'production',
     module: {
         rules:[
             {
